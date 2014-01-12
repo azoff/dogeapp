a3770c52bed4c143d4b0cd42460b723062f4edb1
Jonathan Azoff <jon@azoffdesign.com>
2014-01-11 19:57:43 -0800

    removed comments

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-// http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=132
-/*
-{
-	success: 1,
-	return: {
-		markets: {
-			DOGE: {
-				lasttradeprice: "0.00000031"
-			}
-		}
-	}
-}
-// https://coinbase.com/api/v1/prices/spot_rate
-/*
-{
-	amount: "814.91",
-	currency: "USD"
-}
-*/
 var http  = require('http');
 var https = require('https');
 var ns    = require('node-static');
